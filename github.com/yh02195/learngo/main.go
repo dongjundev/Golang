8dcc8d86fc5886fc70ff0be02878c947efd0978d
dongjun <yh02195@gmail.com>
2021-09-05 18:28:48 +0900

    .

--- github.com/yh02195/learngo/main.go
+++ github.com/yh02195/learngo/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	// length, upperName := lenAndUpper("dongjun")
+	// fmt.Println(length, upperName)
+	repeatMe("jun", "ho", "woong", "jin")
 }
